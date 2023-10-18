81e4b907c007ae4a95f6e94464f07ee7ecaf0c11
MD Sayedul Haque <mdsayedulhaque932@gmail.com>
2023-10-18 02:58:11 -0700

    Banner section added in the Home Page

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100  rounded-box w-52">
                         {navLinks}
                     </ul>
                 </div>
@@ -28,7 +28,7 @@ const Navbar = () => {
                 </ul>
             </div>
             <div className="navbar-end">
-                <NavLink><button className="btn">Log in</button></NavLink>
+                <NavLink><button className="px-4 py-2 rounded-sm font-semibold bg-sky-400">Log in</button></NavLink>
             </div>
         </div>
     );
