c0f1002a861cb4620684e3ff8247007e8439f0fb
MD Sayedul Haque <mdsayedulhaque932@gmail.com>
2023-10-18 06:00:46 -0700

    Brand data get from Database and show brand photo and brand name in the Home Page of the client side

--- src/components/AllBrand/AllBrand.jsx
+++ src/components/AllBrand/AllBrand.jsx
@@ -7,7 +7,7 @@ const AllBrand = () => {
 
     return (
         <div>
-            <h1 className="text-4xl font-semibold text-center my-10">All Brand</h1>
+            <h1 className="text-4xl font-semibold text-center my-10">Brands</h1>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7">
                 {
                     allBrand.map(brand => <Brand key={brand._id} brand={brand}></Brand>)
