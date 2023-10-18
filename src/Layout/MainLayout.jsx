6d4c528ce0f89d9a207d5bc66d1e4ee13817550d
MD Sayedul Haque <mdsayedulhaque932@gmail.com>
2023-10-18 02:30:54 -0700

    Navbar create and responsive for all devices

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -3,7 +3,7 @@ import Navbar from "../components/Navbar/Navbar";
 
 const MainLayout = () => {
     return (
-        <div>
+        <div className="max-w-[1250px] mx-auto my-2">
             <Navbar></Navbar>
             <Outlet></Outlet>
         </div>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,35 @@
-
+import { NavLink } from "react-router-dom"
 
 const Navbar = () => {
+    const navLinks = <>
+        <li> <NavLink to="/">Home</NavLink></li>
+        <li> <NavLink to="/addProduct">Add Product</NavLink></li>
+        <li><NavLink to="/myCart">My Cart</NavLink></li>
+    </>
     return (
-        <div>
-            <h1>This is Navbar</h1>
+        <div className="navbar bg-base-100">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                        {navLinks}
+                    </ul>
+                </div>
+                <div className="flex justify-center items-center">
+                    <img className="w-[40px] h-[40px] hidden lg:flex" src="logo.jpg" alt="" />
+                    <a className="btn btn-ghost normal-case text-xl">TechWizPro</a>
+                </div>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    {navLinks}
+                </ul>
+            </div>
+            <div className="navbar-end">
+                <NavLink><button className="btn">Log in</button></NavLink>
+            </div>
         </div>
     );
 };
