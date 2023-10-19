086590b4a719c1376ecab620b79a53460b7a01e7
MD Sayedul Haque <mdsayedulhaque932@gmail.com>
2023-10-18 20:01:00 -0700

    User created by email and password Authentication

--- src/Page/Register/Register.jsx
+++ src/Page/Register/Register.jsx
@@ -1,7 +1,26 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../Provider/AuthProvider";
 
 
 const Register = () => {
+
+    const {userCreate} = useContext(AuthContext)
+    const handleSignUp = (e) => {
+        e.preventDefault()
+        const form = e.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password);
+
+        userCreate(email, password)
+        .then(result => {
+            console.log(result.user);
+        })
+        .catch(error => {
+            console.error(error);
+        })
+    }
     return (
         <div className="hero min-h-screen bg-sky-50">
         <div className="hero-content flex-col">
@@ -9,18 +28,18 @@ const Register = () => {
                 <h1 className="text-5xl font-bold">Register now!</h1>
             </div>
             <div className="card flex-shrink-0 w-full shadow-2xl bg-base-100">
-                <form className="card-body">
+                <form onSubmit={handleSignUp} className="card-body">
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text">Email</span>
                         </label>
-                        <input type="email" placeholder="email" className="input input-bordered" required />
+                        <input type="email" placeholder="email" name="email" className="input input-bordered" required />
                     </div>
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text">Password</span>
                         </label>
-                        <input type="password" placeholder="password" className="input input-bordered" required />
+                        <input type="password" name="password" placeholder="password" className="input input-bordered" required />
                     </div>
                     <div className="form-control mt-6">
                         <button className="w-full bg-sky-400 py-2 font-semibold text-xl text-white">Register</button>
--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,11 +1,21 @@
 import PropTypes from "prop-types"
 import { createContext } from "react";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import app from "../firebase/firebase.config";
 
-const AuthContext = createContext()
+
+const auth = getAuth(app);
+export const AuthContext = createContext()
+
 const AuthProvider = ({ children }) => {
 
-    const authInfo = {
 
+    const userCreate = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    const authInfo = {
+        userCreate,
     }
     return (
         <AuthContext.Provider value={authInfo}>
